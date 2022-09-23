fbe59975314a4a1030b7a9b80fd3188abc33f60a
Alejandra-pinzon-camelo <lalejandrapinzon@ucundinamarca.edu.co>
2022-09-23 18:31:31 -0500

    se agrega segundo punto

---
+++ src/operacionabstracta/Division.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operacionabstracta;
+
+/**
+ *
+ * @author aleja
+ */
+public class Division implements Iinterface{
+
+    @Override
+    public double Numero(double n,double m) {
+        n=n/m;
+    return n;
+    }
+    
+}
---
+++ src/operacionabstracta/Iinterface.java
@@ -0,0 +1,10 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operacionabstracta;
+
+public interface Iinterface {
+    public double Numero(double n,double m);
+}
